bf370043dc781e0a4c206a91d9259d58b829f075
tirq <tiagogelatti@outlook.com>
2019-12-29 19:36:50 +0100

    Updates

---
+++ precache-manifest.03c19894934e77bf350ddec727fde9a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb301bf86dfc0d28d38f95f7f2252159",
+    "url": "/learning-react-the-complete-Guide-2nd-assignment/index.html"
+  },
+  {
+    "revision": "cc05b9da4ca4dd9c329f",
+    "url": "/learning-react-the-complete-Guide-2nd-assignment/static/css/main.16f53098.chunk.css"
+  },
+  {
+    "revision": "6e4eb3fa72f2e050e654",
+    "url": "/learning-react-the-complete-Guide-2nd-assignment/static/js/2.57b5a902.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/learning-react-the-complete-Guide-2nd-assignment/static/js/2.57b5a902.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc05b9da4ca4dd9c329f",
+    "url": "/learning-react-the-complete-Guide-2nd-assignment/static/js/main.e0a19a0f.chunk.js"
+  },
+  {
+    "revision": "aef0d3148e92aca50579",
+    "url": "/learning-react-the-complete-Guide-2nd-assignment/static/js/runtime-main.8c709e0c.js"
+  }
+]);
